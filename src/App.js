2a916c6bf345d852c70a3e96012109e150a57eb9
Ogheneovo Eko <ovoeko@gmail.com>
2022-09-28 14:02:07 +0100

    chat ui set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState("")
+  const [text, setText] = useState("")
+
+  const handleSubmit = () => {
+    const enteredText = text.trim()
+    if (enteredText.length === 0) return 
+      if (!name) {
+        setName(enteredText)
+        setText("")
+        return
+      }
+      
+  }
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSubmit()
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='title'>Simple Chat</h1>
+      <main className='chatbox'>
+        <header className='App-header'>
+          Welcome to the chatbox <strong>{name.toLocaleUpperCase()}</strong>
+        </header>
+        <div className='chat-content'>
+        </div>
+          <div className='chat-controls'>
+            <input 
+              type='text'
+              value={text}
+              onKeyDown={handleKeyPress}
+              onChange={(e) => setText(e.target.value)}
+              placeholder={!name ? 'Enter your name': 'Enter your message'}
+            />
+            <button
+            onClick={handleSubmit}
+            >Send</button>
+          </div>
+      </main>
     </div>
   );
 }
