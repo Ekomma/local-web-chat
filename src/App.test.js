a9f424b19eee7e1a4b18fe55a7f527f602c3e505
Ogheneovo Eko <ovoeko@gmail.com>
2022-09-29 06:19:50 +0100

    code formatting

--- src/App.test.js
+++ src/App.test.js
@@ -1,26 +1,26 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/Welcome to the chatbox/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test('saves user name', async () => {
+test("saves user name", async () => {
   render(<App />);
-  const name = 'John'
-  const input = await screen.findByPlaceholderText('Enter your name')
-  fireEvent.change(input, {target: {value: name}})
-  fireEvent.click(screen.getByText(/send/i, {selector: 'button'}))
-  expect(screen.getByText(/John/i)).toBeInTheDocument()
-})
+  const name = "John";
+  const input = await screen.findByPlaceholderText("Enter your name");
+  fireEvent.change(input, { target: { value: name } });
+  fireEvent.click(screen.getByText(/send/i, { selector: "button" }));
+  expect(screen.getByText(/John/i)).toBeInTheDocument();
+});
 
-test('message in chat box', async () => {
+test("message in chat box", async () => {
   render(<App />);
-  const value = 'This is a random message'
-  const input = await screen.findByPlaceholderText('Enter your message')
-  fireEvent.change(input, {target: { value }})
-  fireEvent.click(screen.getByText(/send/i, {selector: 'button'}))
-  expect(screen.getByText(value)).toBeInTheDocument()
-})
+  const value = "This is a random message";
+  const input = await screen.findByPlaceholderText("Enter your message");
+  fireEvent.change(input, { target: { value } });
+  fireEvent.click(screen.getByText(/send/i, { selector: "button" }));
+  expect(screen.getByText(value)).toBeInTheDocument();
+});
